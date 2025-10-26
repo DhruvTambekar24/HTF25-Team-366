0d5e9563bded4811b9d566262ab621e03ca2bcf8
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-10-26 14:45:26 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NavbarDemo from "@/components/NavBarDemo";
+import { Web3Provider } from "@/context/Web3Context";
+import { Analytics } from "@vercel/analytics/next"
+const geistSans = Montserrat({
+  variable: "--font-monsterrat",
+  subsets: ["latin"],
+});
+
+const geistMono = Montserrat({
+  variable: "--font-monsterrat",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MintMyTicket – Decentralized Event Ticketing",
+  description: "Mint, manage, and verify event tickets on-chain using MintMyTicket – a blockchain-powered ticketing platform ensuring transparency and security.",
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100`}
+      >
+        <Web3Provider>
+             <NavbarDemo />
+          {/* <Header /> */}
+          {children}
+          {/* <Footer /> */}
+          <Analytics />
+        </Web3Provider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import CTA from "./components/CTA";
+import HeroSection from "./components/HeroSection";
+import LandingPage from "./components/LandingPage";
+
+export default function Home() {
+  return (
+    <div className="">
+
+      {/* <HeroSection />
+      <CTA/> */}
+
+      <LandingPage />
+
+    </div>
+  );
+}
